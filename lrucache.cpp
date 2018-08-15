0dd69ac46b1901fa66cd9b8f4a9bef4f03c46fbb
mrsaurabhsrivastava <mr.saurabh.srivastava@gmail.com>
2018-08-15 14:07:01 +0530

    adding new repo for general

---
+++ lrucache.cpp
@@ -0,0 +1,16 @@
+//
+#include<iostream>
+#include<vector>
+#include<climits>
+#include<algorithm>
+#include<string>
+#include<cstring>
+#include<cmath>
+#define FOR(i,n) for(int i = 0; i < n; i++)
+
+using namespace std;
+
+int main() 
+{
+	std::ios_base::sync_with_stdio(false);
+}
